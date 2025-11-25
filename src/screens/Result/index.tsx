a1e4d486fb70cef26703a5d130b5c5ba123c2f69
simone <sgmartins80@gmail.com>
2025-11-25 20:05:35 -0300

    Atualiza ResultScreen e adiciona arquivo de estilos

--- src/screens/Result/index.tsx
+++ src/screens/Result/index.tsx
@@ -1,11 +1,73 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, StyleSheet } from 'react-native';
+//import from '../context/QuizContext';//
 
-export default function ResultScreen({ navigation }: any) {
+export default function ResultScreen({ route, navigation }: any) {
+  const { score, playerName } = route.params; //  nome e pontuação
+
+  const handleSave = async () => {
+    const newRecord = {
+      name: playerName,
+      score,
+      date: new Date().toISOString(),
+    };
+
+    try {
+      await fetch(
+        "https://690a7b0d1a446bb9cc22a902.mockapi.io/Registro_Pontuacao",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(newRecord),
+        }
+      );
+
+      navigation.navigate("Ranking");
+    } catch (error) {
+      console.error("Erro ao salvar:", error);
+    }
+  };
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Tela Resultado (Simone vai trabalhar aqui)</Text>
-      <Button title="Ir para Ranking" onPress={() => navigation.navigate('Ranking')} />
+    <View style={styles.container}>
+      <Text style={styles.title}>Resultado</Text>
+
+      <Text style={styles.player}>Jogador: {playerName}</Text>
+
+      <Text style={styles.score}>Você fez {score} pontos!</Text>
+
+      <Button title="Salvar Pontuação" onPress={handleSave} />
+
+      <View style={{ height: 20 }} />
+
+      <Button
+        title="Ir para Ranking"
+        onPress={() => navigation.navigate("Ranking")}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20
+  },
+  title: {
+    fontSize: 26,
+    fontWeight: "bold",
+    marginBottom: 12
+  },
+  player: {
+    fontSize: 20,
+    marginBottom: 10
+  },
+  score: {
+    fontSize: 20,
+    marginBottom: 20
+  }
+});
---
+++ src/screens/Result/styles.ts
@@ -0,0 +1,21 @@
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20
+  },
+  title: {
+    fontSize: 26,
+    fontWeight: "bold",
+    marginBottom: 12
+  },
+  player: {
+    fontSize: 20,
+    marginBottom: 10
+  },
+  score: {
+    fontSize: 20,
+    marginBottom: 20
+  }
+});
